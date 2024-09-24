4ce0414960e8184501f8d79f2d3f89fcc8b5d3f7
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-09-24 18:57:34 +0630

    ssl error fix

--- config/ssl_option.js
+++ config/ssl_option.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const sslOptions = {
-  key: fs.readFileSync("/etc/n4/stunnel.pem"),
-  cert: fs.readFileSync("/etc/n4/stunnel.pem"),
+  key: fs.readFileSync(" /etc/letsencrypt/live/web.n4key.xyz/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/web.n4key.xyz/fullchain.pem"),
 };
 
 module.exports = { sslOptions };
